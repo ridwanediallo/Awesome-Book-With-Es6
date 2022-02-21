04268035e583866fe6be10e9c86698bc0711bccb
Ridwan <ridwan>
2022-02-21 08:17:45 +0000

    Fix linters errorst

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import Library from "./modules/library.js";
 import navigation from "./modules/navbar.js";
 /* eslint-enable */
 
-import { DateTime } from "./node_modules/luxon/src/luxon.js";
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
-const dateEl = document.querySelector(".date");
+const dateEl = document.querySelector('.date');
 
 /* eslint-disable */
 const library = new Library();
